7f5f805fbf4c6451aff60e4251913685b2e51ba6
JuanjoCieri <juin@hotmail.com.ar>
2022-09-26 13:54:46 -0300

    Final

--- api/src/app.js
+++ api/src/app.js
@@ -1,26 +1,44 @@
-import express from "express"
-import cors from "cors"
-import {pool} from "./db.js"
-// import routes from './routes/index.js'
-// import {routes} from './routes/index.js'
+import express from "express";
+import cors from "cors";
+import { pool } from "./db.js";
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.get("/categories", async (req, res) => {
-    const [products] = await pool.query('SELECT * FROM category')
-    res.json(products)
-})
+  try {
+    const [products] = await pool.query("SELECT * FROM category");
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.get("/categories/:id", async (req, res) => {
-    const {id} = req.params
-    const [products] = await pool.query(`SELECT * FROM product WHERE category = ${id}`)
-    res.json(products)
-})
+  try {
+    const { id } = req.params;
+    const [products] = await pool.query(
+      `SELECT * FROM product WHERE category = ${id}`
+    );
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-// app.use('/', routes);
+app.get("/categories/name/:name", async (req, res) => {
+  try {
+    const { name } = req.params;
+    const [products] = await pool.query(
+      `SELECT * FROM product WHERE name LIKE "%${name}%"`
+    );
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-app.listen(3000)
+app.listen(3000);
 
-console.log("Server listening")
+console.log("Server listening");
--- client/app.js
+++ client/app.js
@@ -1,61 +1,79 @@
-// const HTMLResponse = document.querySelector("#app");
-// const HTMLResponse2 = document.querySelector("#asd");
+function search() {
+  const value = document.querySelector("#search").value;
+  fetch("http://localhost:3000/categories/name/" + value.toUpperCase())
+    .then((response) => response.json())
+    .then((cat) => {
+      const pro = cat
+        .map(
+          (d) =>
+          `
+          <div class="w-[300px] h-[450px] bg-white shadow-xl">
+          <img src=${
+            d.url_image
+              ? d.url_image
+              : "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1902/pavelstasevich190200120/124934975-no-hay-icono-de-imagen-disponible-vector-plano.jpg?ver=6"
+          } class="w-[300px] h-[300px]" width="300px" h-"300px"/>
+          <div class="flex bg-red-300 h-[150px] flex-col justify-between">
+            <div>
+              ${d.name.toUpperCase()}
+            </div>
+            <div class="h-auto">
+              $ ${d.price}
+            </div>
+          </div>
+          </div>
+          `
+        )
+        .join("");
+      console.log(pro);
+      document.querySelector("#asd").innerHTML =
+        pro.length > 0
+          ? pro
+          : `<div class="w-screen h-screen flex justify-center items-center"><h1>No se encontraron resultados!</h1></div>`;
+    });
+}
 
-// function click (id) {
-//     fetch("http://localhost:3000/categories/" + id)
-//   .then((response) => response.json())
-//   .then((cat) => {
-//     const pro = cat.map((d) => `<li>${d.name}</li>`)
-//     console.log(pro)
-//     document.querySelector("#asd").innerHTML = `<ul>${pro}</ul>`
-//   });
-// }
-
-function clickc(id) {
-  const idd = console.log(id);
+function products (id) {
   fetch("http://localhost:3000/categories/" + id)
     .then((response) => response.json())
     .then((cat) => {
-      const pro = cat.map(
-        (d) =>
-          `
-          <div class="w-[300px] h-[450px] bg-white">
+      const pro = cat
+        .map(
+          (d) =>
+            `
+          <div class="w-[300px] h-[450px] bg-white shadow-xl">
           <img src=${
             d.url_image
-          } class="w-[300px] h-[300px]" width="300px"/>
-          <div class="flex flex-col">${d.name.toUpperCase()} $ ${d.price}</div>
+              ? d.url_image
+              : "https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1902/pavelstasevich190200120/124934975-no-hay-icono-de-imagen-disponible-vector-plano.jpg?ver=6"
+          } class="w-[300px] h-[300px]" width="300px" h-"300px"/>
+          <div class="flex h-[150px] flex-col justify-between">
+            <div>
+              <h1>${d.name.toUpperCase()}</h1>
+            </div>
+            <div class="h-full ">
+              <h3 class="text-orange-700">$ ${d.price}</h3>
+            </div>
+          </div>
           </div>
           `
-      ).join("");
-      console.log(pro);
+        )
+        .join("");
+      console.log(cat);
       document.querySelector("#asd").innerHTML = pro;
     });
 }
 
-// document.getElementById("click").onclick = click;
-
-// function get (id) {
-//   const d = console.log(id)
-//   HTMLResponse.innerHTML = d
-// }
-
-// const id = 1
-
-// HTMLResponse.innerHTML = console.log(id)
-
-// function add (id) {
-//   const d = console.log(id)
-//   HTMLResponse.innerHTML = d
-// }
-
 fetch("http://localhost:3000/categories")
   .then((response) => response.json())
   .then((cat) => {
-    const categories = cat.map(
-      (d) =>
-        `<button onclick="clickc(${d.id})">${d.name.toUpperCase()}</button>`
-    );
-    console.log(categories);
-    // HTMLResponse.innerHTML = `<div class="flex flex-col gap-5">${categories}</div>`
+    const categories = cat
+      .map(
+        (d) =>
+          `<button class="border w-full h-32 flex items-center pl-5 text-sm lg:text-xl" onclick="products(${
+            d.id
+          })">${d.name.toUpperCase()}</button>`
+      )
+      .join("");
     document.querySelector("#app").innerHTML = categories;
   });
